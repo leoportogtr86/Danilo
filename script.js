68b930708d5aad383621ec6d788609b5b73f9767
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-02-16 18:13:34 -0300

    small update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let naoConcluido3 = document.getElementById('naoConcluido3')
 let naoConcluido4 = document.getElementById('naoConcluido4')
 let naoConcluido5 = document.getElementById('naoConcluido5')
 
-semana2.style.display = 'none'
+
 
 semana2.onclick = function () {
 
